a666ae6d071fce426291a02ccebef6e0fca74bb3
Thathsara Amarakoon <amarakoon.thathsara@gmail.com>
2025-10-18 07:23:25 +1300

    Initial Commit

---
+++ BNZDemo/BNZDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BNZDemoApp.swift
+//  BNZDemo
+//
+//  Created by Thathsara Amarakoon on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BNZDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
